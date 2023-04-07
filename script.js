d732853804ef42e16c216175a05fd7e000eb0ad7
chahatjain2527 <chahatjain2527@gmail.com>
2023-04-07 23:04:34 +0530

    added more code and correct the old code

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const boxes = document.querySelectorAll("box");
-const gameBox = document.querySelector("gameBox");
-const btn = document.querySelector("btn");
+// "Use Strict";
+// const boxes = document.querySelectorAll(".box");
+// const instructions = document.querySelector(".instructions");
+// const btn = document.querySelector(".btn");
 
-let currPlayer;
-let gameGrid;
+// let currPlayer;
+// let gameGrid;
 
-const winPos = { 
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,5]
-};
+// const winPos = [ 
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,5]
+// ];
 
 function Initial(){
-    currPlayer = "X";
+    currPlayer = "O";
     gameGrid = ["","","","","","","","",""];
-    btn.classList.remove("active");
-    gameBox.innerText = `Current Player - ${currPlayer}`;
+    // btn.classList.remove("active");
+    // instructions.innerText = `Current Player `;
+    document.getElementsByClassName('instructions');
+    console.log(document.getElementsByClassName('instructions').length());
 }
 
 Initial();
